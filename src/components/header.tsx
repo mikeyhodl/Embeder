e0a0cde2651196c12182943edff2846b84a50f19
mikey <mike@mikeowino.com>
2025-04-20 11:58:54 +0300

    Refactor Header component to use Next.js Link for navigation and update logo SVG styling

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,14 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="text-gray-600 body-font bg-slate-100 sticky top-0 z-40">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-        <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto">
-          {/* <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="hover:text-gray-900">Fourth Link</a> */}
-        </nav>
-        <a className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0">
+        <nav className="flex lg:w-2/5 flex-wrap items-center text-base md:ml-auto"></nav>
+        <Link
+          href="/"
+          className="flex order-first lg:order-none lg:w-1/5 title-font font-medium items-center text-gray-900 lg:items-center lg:justify-center mb-4 md:mb-0"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -16,13 +16,13 @@ export default function Header() {
             strokeLinecap="round"
             strokeLinejoin="round"
             strokeWidth={2}
-            className="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full"
+            className="w-10 h-10 text-white p-2 bg-blue-500 rounded-full"
             viewBox="0 0 24 24"
           >
-            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5" />
+            <path d="M5 3l14 9-14 9V3z" />
           </svg>
           <span className="ml-3 text-xl">Video Player</span>
-        </a>
+        </Link>
         <div className="lg:w-2/5 inline-flex lg:justify-end ml-5 lg:ml-0"></div>
       </div>
     </header>
