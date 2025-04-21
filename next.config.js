24fffe180f50f3c5b193f95ea20ddffb68b67550
Mike Owino <mike.owino40@yahoo.com>
2025-04-21 09:11:57 +0000

    add experimental

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {};
 module.exports = nextConfig;
 
 module.exports = {
+  experimental: {
+    optimizePackageImports: [
+      '@prisma/client',
+    ]
+  },
   images: {
     domains: [
       "images.unsplash.com",
