18ebe8662f3379fa4f339439631e19879ff4f66e
mikey <mike.owino40@yahoo.com>
2025-05-27 16:31:27 +0300

    rm prisma

--- prisma/migrations/20250420171529_init/migration.sql
+++
@@ -1,5 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Video" DROP CONSTRAINT "Video_playlistId_fkey";
-
--- AddForeignKey
-ALTER TABLE "Video" ADD CONSTRAINT "Video_playlistId_fkey" FOREIGN KEY ("playlistId") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
